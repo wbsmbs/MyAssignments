f622ae17319551d5088d415f022d93c0a25cf5f8
wbsmbs <162807529+wbsmbs@users.noreply.github.com>
2024-03-09 18:29:50 +0530

    Add files via upload
    Week1 Day 1 Assignments

---
+++ week1/Firefox.java
@@ -0,0 +1,17 @@
+package week1;
+
+public class Firefox {
+	public static void main(String[] args) {
+		int releaseYear = 1998;
+		String browserName = "Firefox";
+		float browserVersion = 100.2f;
+		boolean isVisible = true;
+		char browserLogo = 'f';
+		
+		System.out.println("The Browser Name is " +browserName + "with browser version" +browserVersion);
+		System.out.println("The Browser Logo is " +browserLogo +"with Release Year" +releaseYear);
+		System.out.println("This is" +isVisible);
+		
+	}
+
+}
---
+++ week1/NewProgram.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class NewProgram {
+	
+	// type main and press Ctrl + Space and then Press Enter
+	public static void main(String[] args) {
+		
+		//Click syso press Ctrl + Space and then Press Enter
+		
+		System.out.println("Welcome to TestLeaf");
+	}
+	
+}
